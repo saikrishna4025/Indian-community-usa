9164806136af6dc4b244ddb618fcdcedbab6beb5
saikrishna4025 <saikrishnabejjanki.4025@gmail.com>
2026-03-20 14:22:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function KrishWebsite() {
   return (
     <div style={{ 
       fontFamily: "Arial", 
-      backgroundImage: "url('https://images.unsplash.com/photo-1534447677768-be436bb09401?q=80&w=1920&auto=format&fit=crop')",
+      backgroundImage: "url('/nyc.jpg')",
       backgroundSize: "cover",
       backgroundPosition: "center",
       color: "white", 
